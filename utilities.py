97f41daeb9b1249d1e7f8ea43389d90cc0c41fdf
TheMrPhantom <diemilbe@gmx.net>
2020-07-04 17:56:39 +0200

    Added sub string searching and some output for the user

--- utilities.py
+++ utilities.py
@@ -12,13 +12,15 @@ class Nextcloud:
         self.oc.login(username, password)
         self.remote_directory=remote_directory
 
-    def get_links(self,lectures,link_expire_in_days=7):
+    def get_links(self,lectures,link_expire_in_days=7,accuracy=7):
         next_week = datetime.datetime.now()+datetime.timedelta(days=link_expire_in_days)
         next_week_string = str(next_week.year)+"-" + str(next_week.month)+"-"+str(next_week.day)
-
+        print("Getting exams from server...")
         files = self.oc.list(self.remote_directory)
+        print("Done...")
         output={}
         for lecture in lectures:
+            print("Searching for:",lecture)
             for f in files:
                 if f.file_type == "dir":
                     split_path=f.path.split("/")
@@ -29,9 +31,10 @@ class Nextcloud:
                     modified_lecture=lecture.replace("-","")
                     
                     distance=stein.distance(modified_lecture, modified_lecture_name_server)
-                    if distance < 10:
+                    if distance < accuracy or modified_lecture.lower() in modified_lecture_name_server.lower():
                         link_info=self.oc.share_file_with_link(
                             f.path, expire_date=next_week_string)
                         output[lecture_name_server]=(link_info.get_link(),1/float(math.sqrt(distance)))
+        print()
         return output
                         
